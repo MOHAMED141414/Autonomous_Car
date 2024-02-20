f3d1e526758b86eb59280d7b335260b9f73c3a9d
MOHAMED141414 <126418845+MOHAMED141414@users.noreply.github.com>
2024-02-20 20:47:51 +0200

    Update TIM1_interface.h

--- Autonomous_Car/TIM1_interface.h
+++ Autonomous_Car/TIM1_interface.h
@@ -13,7 +13,7 @@ void TMR1_Voidstart(void);
 void TMR1_Voidstop (void);
 
 void TMR1_VoidsetCompareMatchValueA (u16 compareValue);
-void TMR1_VoidsetFastPWM_usingMode14(f32 dytyCycle, u16 freq);
+
 
 
 
